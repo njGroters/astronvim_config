15e8ac63ccd4752c3707c7786792d7e20721e658
Nicholas Groters <nicholas.groters@tekton.com>
2025-07-30 10:37:48 -0400

    Add snacks config + Enable explorer

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "folke/snacks.nvim",
+    enabled = true,
+    ---@type snacks.Config
+    opts = {
+      explorer = {
+        -- your explorer configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      },
+      picker = {
+        sources = {
+          explorer = {
+            -- your explorer picker configuration comes here
+            -- or leave it empty to use the default settings
+          },
+        },
+      },
+    },
+  },
+}
