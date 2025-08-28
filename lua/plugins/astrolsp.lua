f803c8a8c18f694645551fe96109f2547383669f
Nicholas Groters <nicholas.groters@tekton.com>
2025-08-28 09:17:47 -0400

    Remove lsp formatting workarounds
    (prettier/esling conflict was fixed!)

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -6,12 +6,4 @@
 return {
   "AstroNvim/astrolsp",
   optional = true,
-  opts = {
-    autocmds = {
-      eslint_fix_on_save = false,
-    },
-    formatting = {
-      async = true,
-    },
-  },
 }
