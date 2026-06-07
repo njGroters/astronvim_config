37b39498508316ff04554301e8ca2cc29fcd67a1
njGroters <njGroters@gmail.com>
2026-06-07 12:59:03 -0400

    Revert lsp config to main

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,7 +1,21 @@
+---@diagnostic disable: missing-fields
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
 
 return {
   "AstroNvim/astrolsp",
-  optional = true,
+  ---@type AstroLSPOpts
+  opts = {
+    config = {
+      vtsls = {
+        settings = {
+          typescript = {
+            format = {
+              enable = false,
+            },
+          },
+        },
+      },
+    },
+  },
 }
